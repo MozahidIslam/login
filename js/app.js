62ecbbabfd5edee5fd4b45c9f89bcb51e196b043
mozahid islam <47480154+MozahidIslam@users.noreply.github.com>
2020-03-31 09:15:02 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+    function check(form){
+        if(form.us.value == "mozahid" && form.pwd.value == "123"){
+         window.open('https://mozahidislam.github.io/portfolio/')
+        } else{
+         alert( " the username and password not correct")
+
+        }
+    }
+ 
